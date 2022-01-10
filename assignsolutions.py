933d7fb61c92f1b5a1a3393deb13de7a67963591
Rithik21103054 <97078673+Rithik21103054@users.noreply.github.com>
2022-01-10 15:33:26 +0530

    Add files via upload

---
+++ assignsolutions.py
@@ -0,0 +1,43 @@
+PS C:\Users\rithi\OneDrive\Desktop\pec python> & C:/Users/rithi/AppData/Local/Programs/Python/Python310/python.exe "c:/Users/rithi/OneDrive/Desktop/pec python/ques1.py"
+the value of a is7
+the value of b is6
+the value of c is8
+7.0
+
+
+PS C:\Users\rithi\OneDrive\Desktop\pec python> & C:/Users/rithi/AppData/Local/Programs/Python/Python310/python.exe "c:/Users/rithi/OneDrive/Desktop/pec python/ques2.py"
+the income of user is50000
+number of dependents are10
+the amount of tax is
+2000.0
+
+
+PS C:\Users\rithi\OneDrive\Desktop\pec python> & C:/Users/rithi/AppData/Local/Programs/Python/Python310/python.exe "c:/Users/rithi/OneDrive/Desktop/pec python/ques3.py"
+enter your sid21103054
+enter your namerithik
+enter your genderM
+enter your course namecse
+enter your cgpa8.76
+[21103054, 'rithik', 'male', 'cse', 8.76]
+
+
+PS C:\Users\rithi\OneDrive\Desktop\pec python> & C:/Users/rithi/AppData/Local/Programs/Python/Python310/python.exe "c:/Users/rithi/OneDrive/Desktop/pec python/ques4.py"
+marks of student_1  78
+marks of student_2  87
+marks of student_3  89
+marks of student_4  98
+marks of student_5  95
+list :
+[78, 87, 89, 98, 95]
+sorted list(decreasing order)
+[98, 95, 89, 87, 78]
+
+
+PS C:\Users\rithi\OneDrive\Desktop\pec python> & C:/Users/rithi/AppData/Local/Programs/Python/Python310/python.exe "c:/Users/rithi/OneDrive/Desktop/pec python/ques5(a).py"
+(a)
+['RED', 'GREEN', 'WHITE', 'BLACK', 'PINK', 'YELLOW']
+['RED', 'GREEN', 'WHITE', 'PINK', 'YELLOW']
+PS C:\Users\rithi\OneDrive\Desktop\pec python> & C:/Users/rithi/AppData/Local/Programs/Python/Python310/python.exe "c:/Users/rithi/OneDrive/Desktop/pec python/ques5(b).py"
+(b)
+['RED', 'GREEN', 'WHITE', 'BLACK', 'PINK', 'YELLOW']
+['RED', 'GREEN', 'WHITE', 'PURPLE', 'PURPLE', 'YELLOW']
